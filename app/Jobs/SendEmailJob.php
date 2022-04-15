2140f5f6ee17f35c3aef8502caef0b91186c2a44
coltrida <coltrida@gmail.com>
2022-04-15 11:36:25 +0200

    prova6

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -34,7 +34,7 @@ class SendEmailJob implements ShouldQueue
      */
     public function handle(TradeService $tradeService)
     {
-        $btcusdt = $tradeService->btcusdt();
-        \Mail::to('coltrida@gmail.com')->send(new WarningMail('pippo', $btcusdt['price']));
+       // $btcusdt = $tradeService->btcusdt();
+        \Mail::to('coltrida@gmail.com')->send(new WarningMail('pippo', 5));
     }
 }
