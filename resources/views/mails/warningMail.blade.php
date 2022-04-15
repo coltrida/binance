f34fc40c88d0de4a39f648f6f453db7bf538edd7
coltrida <coltrida@gmail.com>
2022-04-15 13:27:57 +0200

    prova15

--- resources/views/mails/warningMail.blade.php
+++ resources/views/mails/warningMail.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Introduction
 
-Il delta di {{$platform}} è: {{$delta}} E POI {{$delta < 4}}
+Il delta di {{$platform}} è: {{$delta}} E POI {{(float)$delta < 4}}
 
 Thanks,<br>
 {{ config('app.name') }}
