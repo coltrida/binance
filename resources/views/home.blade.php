6ffa55f2579106bac348dde6b43ad9cfc8fc3857
coltrida <coltrida@gmail.com>
2022-04-16 11:20:36 +0200

    modifica dicitura su home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,20 @@
         <div class="row">
             <div class="col-12 col-md-6 col-lg-6">
                 <p class="lead">
-                    <a href="{{route('trade.index')}}" style="width: 250px" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Trades</a>
+                    <a href="{{route('trade.index')}}" style="width: 250px"
+                       class="btn btn-lg btn-secondary fw-bold border-white bg-white">Staking</a>
                 </p>
             </div>
             <div class="col-12 col-md-6 col-lg-6">
                 <p class="lead">
-                    <a href="{{route('platform.index')}}" style="width: 250px" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Inserisci Piattaforma</a>
+                    <a href="{{route('platform.index')}}" style="width: 250px"
+                       class="btn btn-lg btn-secondary fw-bold border-white bg-white">Inserisci Piattaforma</a>
                 </p>
             </div>
             <div class="col-12 col-md-6 col-lg-6">
                 <p class="lead">
-                    <a href="{{route('coin.index')}}" style="width: 250px" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Portafoglio coins</a>
+                    <a href="{{route('coin.index')}}" style="width: 250px"
+                       class="btn btn-lg btn-secondary fw-bold border-white bg-white">Portafoglio coins</a>
                 </p>
             </div>
         </div>
